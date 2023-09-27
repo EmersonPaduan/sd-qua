b71a18e4e83c17174c18d7f28deedb0c47de3cd7
Emerson S. Paduan <emerson.paduan@gmail.com>
2023-09-27 20:25:06 -0300

    security parte 1

---
+++ veiculos/src/main/java/br/anhembi/veiculos/configuration/SecurityConfig.java
@@ -0,0 +1,37 @@
+package br.anhembi.veiculos.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    
+    @Bean
+    InMemoryUserDetailsManager inMemoryUser() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+            .username("user")
+            .password("abc123")
+            .roles("USER")
+            .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests( (autorize) -> autorize
+                .requestMatchers("/login").permitAll() 
+                .requestMatchers("/car").authenticated() 
+                .requestMatchers("/car/delete").hasAnyRole("ADMIN") 
+                .anyRequest().authenticated()
+                ).httpBasic(Customizer.withDefaults());
+        return http.build();   
+    }
+}
---
+++ veiculos/src/main/java/br/anhembi/veiculos/security/AppUser.java
@@ -0,0 +1,5 @@
+package br.anhembi.veiculos.security;
+
+public class AppUser {
+    
+}
